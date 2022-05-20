354a7f9b2fa11d3d20b049022b5da4714a4e295e
the furious dev <so@tfwno.gf>
2022-05-20 10:32:24 +0800

    update datepicker

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,8 @@ import {
   Routes,
   Route
 } from 'react-router-dom';
-import DateFnsUtils from '@date-io/date-fns';
-import LocalizationProvider from '@mui/lab/LocalizationProvider';
+import {AdapterDateFns} from '@mui/x-date-pickers/AdapterDateFns';
+import {LocalizationProvider} from '@mui/x-date-pickers/LocalizationProvider';
 import {Login} from './pages/Login';
 import {Dashboard} from './pages/Dashboard/Dashboard';
 import {ApplicationProvider} from './providers/ApplicationProvider/ApplicationProvider';
@@ -13,19 +13,19 @@ import {defaultTheme} from './themes/defaultTheme';
 
 function App() {
   return (
-    <ThemeProvider theme={defaultTheme}>
-      <ApplicationProvider>
-        <LocalizationProvider dateAdapter={DateFnsUtils}>
-          <BrowserRouter basename={process.env.PUBLIC_URL}>
-            <Routes>
-              <Route path="/" element={<Dashboard />}
-              />
-              <Route path="/login" element={<Login />} />
-            </Routes>
-          </BrowserRouter>
-        </LocalizationProvider>
-      </ApplicationProvider>
-    </ThemeProvider>
+    <LocalizationProvider dateAdapter={AdapterDateFns}>
+      <ThemeProvider theme={defaultTheme}>
+        <ApplicationProvider>
+            <BrowserRouter basename={process.env.PUBLIC_URL}>
+              <Routes>
+                <Route path="/" element={<Dashboard />}
+                />
+                <Route path="/login" element={<Login />} />
+              </Routes>
+            </BrowserRouter>
+        </ApplicationProvider>
+      </ThemeProvider>
+    </LocalizationProvider>
   );
 }
 
--- src/components/DatePicker/DatePicker.types.ts
+++
@@ -1,3 +0,0 @@
-import { DatePickerProps as MuiDatePickerProps } from '@mui/lab/DatePicker';
-
-export interface DatePickerProps extends Omit<MuiDatePickerProps, 'renderInput'> {}
